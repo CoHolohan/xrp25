97f76f076f639bdfccd32c2938f93809c983084a
CoHolohan <cormacholohanc2@gmail.com>
2025-12-16 23:04:05 +0000

    Adding sounds into the game for scores

--- Assignment/ge-assignment/score_label.gd
+++ Assignment/ge-assignment/score_label.gd
@@ -1,5 +1,8 @@
 extends Label3D
 
+@export var score_sounds: Array[AudioStream] = []
+@onready var score_player: AudioStreamPlayer3D = get_node_or_null("ScoreSound")
+
 var score: int = 0
 
 func _ready() -> void:
@@ -8,3 +11,8 @@ func _ready() -> void:
 func _on_vegetable_sliced(_veg: Vegetable) -> void:
 	score += 1
 	text = str(score)
+	
+	if score_player and not score_sounds.is_empty():
+		score_player.stream = score_sounds[randi_range(0, score_sounds.size() - 1)]
+		score_player.pitch_scale = randf_range(0.98, 1.02)
+		score_player.play()
