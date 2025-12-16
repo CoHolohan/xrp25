7c81831a6c8de42e9e3d9ef7c424c04394ebdf2d
CoHolohan <cormacholohanc2@gmail.com>
2025-12-16 23:32:20 +0000

    added more sounds and debugged others

--- Assignment/ge-assignment/floor.gd
+++ Assignment/ge-assignment/floor.gd
@@ -1,6 +1,9 @@
 # FloorKillZone.gd
 extends Area3D
 
+@export var miss_sounds: Array[AudioStream] = []
+@onready var miss_player: AudioStreamPlayer3D = get_node_or_null("MissSound")
+
 func _ready() -> void:
 	monitoring = true
 	monitorable = true
@@ -9,4 +12,13 @@ func _ready() -> void:
 func _on_body_entered(body: Node) -> void:
 	if body is Vegetable:
 		print("Vegetable hit floor and was removed")
+		var veg: Vegetable = body
+		
+		if not veg.is_sliced:
+			if miss_player and not miss_sounds.is_empty():
+				miss_player.global_position = veg.global_position
+				miss_player.stream = miss_sounds[randi_range(0, miss_sounds.size() - 1)]
+				miss_player.pitch_scale = randf_range(0.9, 1.1)
+				miss_player.play() 
+			
 		body.queue_free()
--- Assignment/ge-assignment/score_label.gd
+++ Assignment/ge-assignment/score_label.gd
@@ -1,6 +1,8 @@
 extends Label3D
 
-@export var score_sounds: Array[AudioStream] = []
+@export var milestone_every: int = 5
+@export var milestone_sounds: Array[AudioStream] = []
+
 @onready var score_player: AudioStreamPlayer3D = get_node_or_null("ScoreSound")
 
 var score: int = 0
@@ -11,8 +13,12 @@ func _ready() -> void:
 func _on_vegetable_sliced(_veg: Vegetable) -> void:
 	score += 1
 	text = str(score)
-	
-	if score_player and not score_sounds.is_empty():
-		score_player.stream = score_sounds[randi_range(0, score_sounds.size() - 1)]
-		score_player.pitch_scale = randf_range(0.98, 1.02)
-		score_player.play()
+
+	# Play sound only on milestones (5, 10, 15, ...)
+	if milestone_every > 0 and score % milestone_every == 0:
+		if score_player:
+			# If you provided multiple sounds, pick a random one
+			if not milestone_sounds.is_empty():
+				score_player.stream = milestone_sounds[randi_range(0, milestone_sounds.size() - 1)]
+			score_player.pitch_scale = randf_range(0.98, 1.02)
+			score_player.play()
